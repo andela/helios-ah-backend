c3a1b1d206304703967d3e73029f423ab36371a2
ah-med <ahmedomosanya@gmail.com>
2018-11-27 18:53:40 +0100

    [Chore  #161779772] setup unit testing

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const { expect } = chai;
+
+describe('Sample test to set uop travis', function() {
+      it('should return true', () => {
+          expect(2).to.equal(2);
+      });
+    });
