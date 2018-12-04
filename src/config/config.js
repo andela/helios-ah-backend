279cf426a71122225e138a8180aab33e88a2d363
Ademola Hussain <demola.hussainin@gmail.com>
2018-12-04 13:14:14 +0100

    #161779783 Create user account (#16)
    [Feature 161779783] Add create user account feature

--- src/config/config.js
+++ src/config/config.js
@@ -8,14 +8,18 @@ const databaseConfig = {
     password: process.env.DB_PASSWORD,
     database: process.env.DEVELOPMENT_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    operatorsAliases: false,
+    dialect: 'postgres',
+    logging: false,
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.TEST_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    operatorsAliases: false,
+    dialect: 'postgres',
+    logging: false,
   },
   production: {
     use_env_variable: 'DB_URL_PRODUCTION'
---
+++ src/utilities/index.js
@@ -0,0 +1,4 @@
+import validateUserInputs from './validateUserInput';
+import cryptData from './cryptData';
+
+export { cryptData, validateUserInputs };
