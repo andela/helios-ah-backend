16fd0557f399d158489066713bd0ac30e4450da7
nwashangai <nwashangai@users.noreply.github.com>
2018-12-04 18:15:11 +0100

    #161779779 Validate user signup/registration input (#15)
    * [feature #161779779] - unit test to validate user input; implement model validation.

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define('Users', {
+  const Users = sequelize.define('Users', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
@@ -8,25 +8,63 @@ export default (sequelize, DataTypes) => {
     },
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [3, 15],
+        is: {
+          args: /^[a-z]+$/i,
+          msg: 'First name must contain only Alphabets'
+        }
+      }
     },
     lastName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 15],
+        is: {
+          args: /^[a-z]+$/i,
+          msg: 'Last name must contain only Alphabets'
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Email must be a valid email ID'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6, 150],
+          msg: 'Password must be more than 5 characters'
+        }
+      }
     },
     image: {
       type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
     },
     bio: {
       type: DataTypes.TEXT,
+      validate: {
+        is: {
+          args: /^[A-Za-z]+[A-Za-z0-9 _.,!?;"']+$/i,
+          msg: 'Bio must contain only valid '
+              + 'characters and must begin with '
+              + 'an alphabet'
+        }
+      }
     },
     isVerified: {
       type: DataTypes.BOOLEAN,
@@ -35,13 +73,22 @@ export default (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
+      validate: {
+        len: [3, 15],
+        is: {
+          args: /^[A-Za-z]+[A-Za-z0-9_]+$/i,
+          msg: 'Username must contain only alphabet, '
+              + 'numbers, and characters  and must begin '
+              + 'with an alphabet'
+        }
+      }
     },
   });
-  User.associate = (models) => {
-    User.hasMany(models.VerificationToken, {
+  Users.associate = (models) => {
+    Users.hasMany(models.VerificationToken, {
       foreignKey: 'userId'
     });
   };
-  return User;
+  return Users;
 };
