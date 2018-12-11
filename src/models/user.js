6dccabcc5d7accd0c963510c4c1bda517e0aa0f4
Chiagoziem Nwadike <chiagoziemnwadike@Chiagoziems-MacBook-Pro.local>
2018-12-11 17:46:10 +0100

    [Feature #161779792] Fix merge conflict

--- src/models/user.js
+++ src/models/user.js
@@ -1,97 +1,98 @@
 import crypter from '../utilities/cryptData';
 
 export default (sequelize, DataTypes) => {
-  const Users = sequelize.define('Users', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 25],
-        is: {
-          args: /^[a-z']+$/i,
-          msg: 'First name must contain only Alphabets'
-        }
-      }
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 25],
-        is: {
-          args: /^[a-z']+$/i,
-          msg: 'Last name must contain only Alphabets'
+  const Users = sequelize.define(
+    'Users', {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 25],
+          is: {
+            args: /^[a-z']+$/i,
+            msg: 'First name must contain only Alphabets'
+          }
         }
       },
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: 'Email must be a valid email ID'
-        }
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [6, 150],
-          msg: 'Password must be more than 5 characters'
-        }
-      }
-    },
-    image: {
-      type: DataTypes.STRING,
-      validate: {
-        isUrl: true
-      }
-    },
-    bio: {
-      type: DataTypes.TEXT,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: 'Bio must not be empty'
-        }
-      }
-    },
-    isVerified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [3, 20],
-        isAlphanumeric: {
-          args: true,
-          msg: 'Username must contain only alphanumeric',
-        }
-      }
-    },
-  },
-  {
-    hooks: {
-      beforeUpdate: async (user) => {
-        user.password = await crypter.encryptData(user.password);
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 25],
+          is: {
+            args: /^[a-z']+$/i,
+            msg: 'Last name must contain only Alphabets'
+          }
+        },
       },
-      beforeCreate: async (user) => {
-        user.password = await crypter.encryptData(user.password);
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Email must be a valid email ID'
+          }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [6, 150],
+            msg: 'Password must be more than 5 characters'
+          }
+        }
+      },
+      image: {
+        type: DataTypes.STRING,
+        validate: {
+          isUrl: true
+        }
+      },
+      bio: {
+        type: DataTypes.TEXT,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Bio must not be empty'
+          }
+        }
+      },
+      isVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 20],
+          isAlphanumeric: {
+            args: true,
+            msg: 'Username must contain only alphanumeric',
+          }
+        }
+      },
+    }, {
+      hooks: {
+        beforeUpdate: async (user) => {
+          user.password = await crypter.encryptData(user.password);
+        },
+        beforeCreate: async (user) => {
+          user.password = await crypter.encryptData(user.password);
+        }
       }
     }
-  });
+  );
   Users.associate = (models) => {
     Users.hasMany(models.Authorize, {
       foreignKey: 'userId',
