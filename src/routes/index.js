84a6334be803741ffbfbddf3d809243d32b22642
andrewinsoul <andrewinsoul@gmail.com>
2018-12-17 09:12:23 +0100

    #161779784 Get all articles  (#28)
    [Feature #161779784] get articles functionality; get user's articles;

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,16 +30,21 @@ const routes = (app) => {
     validateUserInputs.validateCreateArticle,
     ArticleController.createArticle
   );
+  app.get(
+    '/api/v1/articles',
+    Authorization.checkToken,
+    ArticleController.getArticles
+  );
+  app.get(
+    '/api/v1/articles/user',
+    Authorization.checkToken,
+    ArticleController.getArticles
+  );
   app.put(
     '/api/v1/articles/:articleId',
     validateUserInputs.validateCreateArticle,
     Authorization.checkToken,
     ArticleController.updateArticle
-  );
-  app.get(
-    '/api/v1/articles',
-    Authorization.checkToken,
-    ArticleController.getArticles,
   );
   app.get(
     '/api/v1/authors',
