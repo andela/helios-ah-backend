34f08bbe89905778028f2247a312898db48b5395
Ademola Hussain <demola.hussainin@gmail.com>
2018-12-18 11:44:11 +0100

    #162701971 Build REST End point for User Login (#36)
    [Feature #162701971] Add login feature

--- src/utilities/helperMethods.js
+++ src/utilities/helperMethods.js
@@ -4,7 +4,7 @@
  * @param {String} message - The error message you want to set.
  * @returns {object} res - The HTTP response object
  */
-const serverError = (res, message) => res.status(500).send({
+const serverError = (res, message) => res.status(500).json({
   success: false,
   message: message || 'Internal server error',
 });
@@ -15,7 +15,7 @@ const serverError = (res, message) => res.status(500).send({
  * @param {object} error - The error object from sequelize.
  * @returns {object} res - The HTTP response object
  */
-const sequelizeValidationError = (res, error) => res.status(400).send({
+const sequelizeValidationError = (res, error) => res.status(400).json({
   success: false,
   message: error.errors[0].message,
 });
@@ -26,7 +26,7 @@ const sequelizeValidationError = (res, error) => res.status(400).send({
  * @param {string} message - Custom message we want to send to the fron-end
  * @returns {object} res - HTTP response object
  */
-const requestSuccessful = (res, message) => res.status(200).send({
+const requestSuccessful = (res, message) => res.status(200).json({
   success: true,
   message: message || 'request completed successfully',
 });
