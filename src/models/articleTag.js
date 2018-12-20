5fe52572a52b4b9a26d87e04c221765895d4a573
jideajayi11 <jideajayi11@gmail.com>
2018-12-20 12:29:34 +0100

    [Feature #161779790] tag an article

--- src/models/articleTag.js
+++ src/models/articleTag.js
@@ -1,9 +1,28 @@
 const articleTagModel = (sequelize, DataTypes) => {
-  const articleTag = sequelize.define('articleTag', {
+  const articleTag = sequelize.define('ArticleTag', {
     id: {
-      type: DataTypes.UUID,
+      allowNull: false,
       primaryKey: true,
-      defaultValue: DataTypes.UUIDV4
+      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.UUID,
+    },
+    articleId: {
+      type: DataTypes.UUID,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Articles',
+        key: 'id',
+        as: 'Articles'
+      }
+    },
+    tagId: {
+      type: DataTypes.UUID,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'tags',
+        key: 'id',
+        as: 'Labels'
+      }
     }
   });
   articleTag.associate = (models) => {
@@ -11,11 +30,12 @@ const articleTagModel = (sequelize, DataTypes) => {
       foreignKey: 'articleId',
       onDelete: 'CASCADE'
     });
-    articleTag.belongsTo(models.tags, {
+    articleTag.belongsTo(models.Tags, {
       foreignKey: 'tagId',
       onDelete: 'CASCADE'
     });
   };
   return articleTag;
 };
+
 export default articleTagModel;
