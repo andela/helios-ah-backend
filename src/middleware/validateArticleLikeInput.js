8c18b2052819a2113736ecc34def339dfe9fa5b4
shegsteham <oluwasegunadepoju@gmail.com>
2019-01-27 13:18:47 +0100

    fix hound violation

--- src/middleware/validateArticleLikeInput.js
+++ src/middleware/validateArticleLikeInput.js
@@ -4,12 +4,10 @@ const validateLikeValue = (req, res, next) => {
   if ((isLiked === true) || (isLiked === false) || (isLiked === undefined)) {
     next();
   } else {
-    return res.status(404).json(
-      {
-        success: false,
-        message: 'Invalid like value'
-      }
-    );
+    return res.status(404).json({
+      success: false,
+      message: 'Invalid like value'
+    });
   }
 };
 
